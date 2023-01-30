44d5e20bac23185ad2166c3d62e5a4bc01b1ab8b
Alexandr Afanasev <116384401+AfanasBlack@users.noreply.github.com>
2023-01-30 21:52:51 +0300

    Commit Task

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Main {
+    private final static String url = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setUserAgent("My Test Service")
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)
+                        .setSocketTimeout(30000)
+                        .setRedirectsEnabled(false)
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(url);
+        CloseableHttpResponse response = httpClient.execute(request);
+        String jsonData = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+
+        List<Data> allData = mapper.readValue(jsonData, new TypeReference<List<Data>>() {});
+        allData.stream().filter(data -> data.getUpvotes() > 0).forEach(System.out::println);
+    }
+}
